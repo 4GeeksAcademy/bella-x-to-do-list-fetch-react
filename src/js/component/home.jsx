98fe35489724836bcdcb72f972f164a6d465a98a
bellaxscobie <bellaxscobie@gmail.com>
2024-09-11 08:05:26 +0000

    created functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+    
+    function addTask() {
+        if (newTask.trim() !== "") {
+            let newTasks = [...tasks, newTask];
+            setTasks(newTasks);
+            setNewTask("");
+        }
+    }
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    function handleEnter(event) {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    }
+
+    function deleteTask(taskIndex) {
+        const updatedTasks = tasks.filter((_, index) => index !== taskIndex);
+        setTasks(updatedTasks);
+    }
+
+    return (
+        <div className='title'>
+            <h1>To-Do List</h1>
+            <div className='container w-50 max-height shade text-center'>
+                <div>
+                    <input 
+                        onChange={(e) => setNewTask(e.target.value)} 
+                        onKeyDown={handleEnter} 
+                        value={newTask} 
+                        type="text" 
+                        placeholder="What's next?" 
+                    />
+                    {tasks.map((task, index) => (
+                        <div className='d-flex justify-content-between' key={index}>
+                            <p className='task-text'>{task}</p>
+                            <button 
+                                onClick={() => deleteTask(index)} 
+                                className="delete-button">
+                                X
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default Home;
+
+
+
